f420911606e0041630174394aa34bb25cb71c5d5
dave <none@none>
2013-10-09 14:43:18 +0100

    First draft version of the python script to calculate R factors Advantages over Fortran version - 1. Almost sane interface 2. Self documenting inputs 3. Should be easy to change into something usable by other scripts directly 4. No restrictions on input size or filename length

---
+++ bin/calculate_rfactor.py
@@ -0,0 +1,60 @@
+#!/bin/env python
+
+# David W. Wright - 08 October 2013
+"""Script to compute the R factor of a comparison between two scattering curves.
+This will usually be one calculated and one experimental data set."""
+
+import argparse
+import numpy
+from sjp_util import sjp_util
+
+def parse_arguments():
+    """Parse command line arguments and ensure correct combinations present"""
+    #Command line option parsing
+    parser = argparse.ArgumentParser(description= 'Compute R factor from a comparison of two scattering surves.')
+    parser.add_argument('-c','--calc', nargs='?', type=str, dest='calc_curve', help = 'Path to the input calculated curve', required=True)
+    parser.add_argument('-x','--expt', nargs='?', type=str, dest='expt_curve', help = 'Path to the input experimental curve', required=True)
+    parser.add_argument('-l','--qmin', nargs='?', type=float, dest='q_min', help = 'Minimum Q value to include in curve fitting', required=True)
+    parser.add_argument('-u','--qmax', nargs='?', type=float, dest='q_max', help = 'Maximum Q value to include in curve fitting', required=True)
+    parser.add_argument('-o','--outfile', nargs='?', type=str, dest='out_file', help = 'Path to the output file', required=False)
+    
+    args = parser.parse_args()
+    return args
+
+# Interpret command line arguments
+args = parse_arguments()
+
+# Load the data from the two input files
+calc_data = numpy.loadtxt(args.calc_curve)
+expt_data = numpy.loadtxt(args.expt_curve)
+
+# Filter inputs to retain only values for Q values between selected q_min and q_max
+idx=(calc_data[:,0] >= args.q_min) & (calc_data[:,0] <= args.q_max)
+calc_data = calc_data[idx]
+
+idx=(expt_data[:,0] >= args.q_min) & (expt_data[:,0] <= args.q_max)
+expt_data = expt_data[idx]
+
+# Create list of calculated I values matched to the nearest experimental Q value
+# Remember that the arrays in python start at 0, those in Fortran at 1
+last_calc = len(calc_data)
+last_expt = len(calc_data)
+
+matched_calc_I = numpy.zeros(last_expt,dtype=float)
+matched_no = sjp_util.qrange_match(expt_data[:,0], calc_data[:,0], calc_data[:,1], last_expt, last_calc, matched_calc_I)
+
+# Calculate averages of the experimental and calculated values over the matched Q range
+
+expt_avg = numpy.mean( expt_data[0:matched_no - 1, 1] )
+calc_avg = numpy.mean( matched_calc_I[0:matched_no - 1] )
+
+# Initial guess of the concentration is the ratio of experimental and average intensities
+con = expt_avg / calc_avg
+
+r_factor = sjp_util.calc_rfactor(expt_data[:,0], expt_data[:,1], matched_calc_I, matched_no, args.q_min, args.q_max, con, False)
+
+if args.out_file == None:
+    print r_factor
+else:
+    with open(args.out_file, "a") as fle:
+        fle.write(args.calc_curve + " " + args.expt_curve + " " + str(args.q_min) + " " + str(args.q_max) + " " + str(r_factor))
