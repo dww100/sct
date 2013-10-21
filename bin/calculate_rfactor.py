35d6777248de125e6937cbf707a9c5cf891b79e3
dave <none@none>
2013-10-21 11:25:11 +0100

    Refactoring - now have a short __main__ calling functions

--- bin/calculate_rfactor.py
+++ bin/calculate_rfactor.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
 # David W. Wright - 08 October 2013
-"""Script to compute the R factor of a comparison between two scattering curves.
+"""Compute the R factor of a comparison between two scattering curves.
 This will usually be one calculated and one experimental data set.
 
 R factor is used by analogy with crystallography where:
@@ -41,9 +41,9 @@ def parse_arguments():
 def load_scatter_curve(filename, q_min, q_max):
     """Load I, Q data from file and filter for values between q_min and q_max"""
     
-    scatter_data = numpy.loadtxt(args.calc_curve)
+    scatter_data = numpy.loadtxt(filename)
     qrange_mask = (scatter_data[:,0] >= q_min) & (scatter_data[:,0] <= q_max)
-    
+
     return scatter_data[qrange_mask]
     
 def match_scatter_curves(target_data, source_data):
@@ -56,77 +56,79 @@ def match_scatter_curves(target_data, source_data):
 
     # Create list of calculated I values matched to the nearest experimental Q value
     # Remember that the arrays in python start at 0, those in Fortran at 1
-    last_calc = len( calc_data )
-    last_expt = len( expt_data )
+    last_source = len( source_data )
+    last_target = len( target_data )
 
     # Initialize array to hold the calculated I values matched to experimental q values
-    matched_calc_I = numpy.zeros( last_expt, dtype=float )
+    matched_I = numpy.zeros( last_target, dtype=float )
     
     # Use the old fortran routine to match the data sets by q value
     # matched_no is the number of datapoints which contain matched data
-    matched_no = sjp_util.qrange_match(expt_data[:,0], calc_data[:,0], 
-                                        calc_data[:,1], last_expt, 
-                                        last_calc, matched_calc_I)
+    matched_no = sjp_util.qrange_match(target_data[:,0], source_data[:,0], 
+                                        source_data[:,1], last_target, 
+                                        last_source, matched_I)
+                                        
+    matched_I.resize(matched_no)
     
-    return matched_no, matched_calc_I
+    return matched_I
 
-# Interpret command line arguments
-args = parse_arguments()
-
-if (not args.header) :
-
-    q_min = args.qrange[0]
-    q_max = args.qrange[1]
-
-    # Load the data from the two input files
-    calc_data = numpy.loadtxt(args.calc_curve)
-    expt_data = numpy.loadtxt(args.expt_curve)
-
-    # Filter inputs to retain only values for Q values between q_min and q_max
-    idx=(calc_data[:,0] >= q_min) & (calc_data[:,0] <= q_max)
-    calc_data = calc_data[idx]
-
-    idx=(expt_data[:,0] >= q_min) & (expt_data[:,0] <= q_max)
-    expt_data = expt_data[idx]
-
-    # Create list of calculated I values matched to nearest experimental Q value
-    # Remember that the arrays in python start at 0, those in our Fortran at 1
-    last_calc = len(calc_data)
-    last_expt = len(expt_data)
-
-    matched_calc_I = numpy.zeros(last_expt,dtype=float)
-    matched_no = sjp_util.qrange_match(expt_data[:,0], calc_data[:,0], 
-        calc_data[:,1], last_expt, last_calc, matched_calc_I)
-
+def calculate_rfactor(target_data, source_data, q_min, q_max):
+    """Compute R factor comparing two matched scattering curves
+    
+    Input data is two I vs Q scattering curves and the min/max Q values to use
+    to compare them
+    Output is the R factor and the scaling factor needed to match I from the 
+    target scattering curve to the source data
+    """
+    
+    matched_source_I = match_scatter_curves(target_data, source_data)
+        
     # Get average I for experimental and calculated values over matched Q range
-
-    expt_avg = numpy.mean( expt_data[0:matched_no - 1, 1] )
-    calc_avg = numpy.mean( matched_calc_I[0:matched_no - 1] )
+    matched_no = len(matched_source_I)
+    expt_avg = numpy.mean( target_data[0:matched_no, 1] )
+    calc_avg = numpy.mean( matched_source_I )
 
     # Initial guess of the concentration:
     # ratio of experimental and calculated average intensities
     con = expt_avg / calc_avg
 
-    r_factor = sjp_util.calc_rfactor( expt_data[:,0], expt_data[:,1], 
-        matched_calc_I, matched_no, q_min, q_max, con, False)
-
+    # Call fortran code to calculate the R factor
+    rfactor = sjp_util.calc_rfactor( expt_data[:,0], expt_data[:,1], 
+        matched_source_I, matched_no, q_min, q_max, con, False)
+    
     # 1/con is the scaling factor needed to multiply experimental I values 
-    # to compare with calculated data
-    scale = 1.0 / con
+    # to compare with calculated data    
+    return rfactor, 1.0/con
 
-    output_data = '{0:s}\t{1:s}\t{2:0.5f}\t{3:0.5f}\t{4:0.5f}\t{5:0.5f}'.format(
-        args.calc_curve, args.expt_curve, q_min, q_max, scale, r_factor)
 
-    if args.out_file == None:
-        print output_data
+if __name__ == "__main__":
+    # Interpret command line arguments
+    args = parse_arguments()
+
+    if (not args.header) :
+        
+        q_min = args.qrange[0]
+        q_max = args.qrange[1]
+
+        # Load the data from the two input files
+        calc_data = load_scatter_curve(args.calc_curve, q_min, q_max)
+        expt_data = load_scatter_curve(args.expt_curve, q_min, q_max)
+
+        rfactor, scale = calculate_rfactor(expt_data, calc_data, q_min, q_max)
+
+        output_data = '{0:s}\t{1:s}\t{2:0.5f}\t{3:0.5f}\t{4:0.5f}\t{5:0.5f}\n'.format(
+            args.calc_curve, args.expt_curve, q_min, q_max, scale, rfactor)
+
+        if args.out_file == None:
+            print output_data
+        else:
+            with open(args.out_file, "a") as fle:
+                fle.write(output_data)
     else:
-        with open(args.out_file, "a") as fle:
-            fle.write(output_data)
-else:
 
-    header = 'Calculated file\tExperimental file\tQ min\tQ max\tScaling factor\tR factor'
-    if args.out_file == None:
-        print header
-    else:
-        with open(args.out_file, "a") as fle:
-            fle.write(header)
+        header = 'Calculated file\tExperimental file\tQ min\tQ max\tScaling factor\tR factor\n'
+        if args.out_file == None:
+            print header
+        else:
+            with open(args.out_file, "a") as fle:
+                fle.write(header)
