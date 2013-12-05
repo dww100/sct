9b28aae7fb620d762de578fa38404999986d74e4
dave <none@none>
2013-12-05 15:13:44 +0000

    calculate_curve: added in smearing code and refactoring

--- bin/calculate_curve.py
+++ bin/calculate_curve.py
@@ -65,50 +65,128 @@ def calc_r_hist(coords, no_bins):
 
     return hist, bin_edges, last_used
 
-def smear_curve(curve):
-    pass
+def spheres_to_sas_curve(coords, radius, q_max, n_points, **kwargs):
 
+    n_bins = kwargs.get('rbins', 400)
 
-args = parse_arguments()
+    q_delta = q_max / n_points
 
-q_delta = args.q_max / args.n_points
+    q = []
+    for n in range(1, n_points + 1):
+        q.append(n * q_delta)
 
-q = []
-for n in range(1, args.n_points + 1):
-    q.append(n * q_delta)
+    natom = len(coords)
 
-res_freq, coords = p2s.read_pdb_atom_data(args.input_filename)
-natom = len(coords)
+    hist, bin_edges, last_used = calc_r_hist(coords, n_bins)
 
-hist, bin_edges, last_used = calc_r_hist(coords, args.n_bins)
+    sigma = []
 
-sigma = []
+    for i in range(0, n_points):
+        sigma_tmp = 0.0
+        for j in range(0, n_bins):
+            qr = q[i] * bin_edges[j + 1]
+            sin_term = np.sin(qr)/ qr
+            sigma_tmp += hist[j] * sin_term
+        sigma.append(sigma_tmp)
 
-for i in range(0, args.n_points):
-    sigma_tmp = 0.0
-    for j in range(0, args.n_bins):
-        qr = q[i] * bin_edges[j + 1]
-        sin_term = np.sin(qr)/ qr
-        sigma_tmp += hist[j] * sin_term
-    sigma.append(sigma_tmp)
+    inv_n = 1.0 / natom
+    inv_n2 = 1.0 / natom**2
 
+    scat = []
 
-inv_n = 1.0 / natom
-inv_n2 = 1.0 / natom**2
+    for i in range(0, n_points):
+        scat.append(sphere_squared_form_factor(q[i], radius)
+                    * (inv_n + 2.0 * inv_n2 * sigma[i]))
 
-scat = []
+    return q, scat
 
-for i in range(0, args.n_points):
-    scat.append(sphere_squared_form_factor(q[i], args.radius)
-                * (inv_n + 2.0 * inv_n2 * sigma[i]))
+def smear_curve(i, q, q_delta, wavelength, spread, divergence):
 
-if args.smear:
-    scat = smear_curve(scat)
+    # Calculation of the sigma for the smearing gaussian
+    # Based on CHAUVIN but 0.25/D^2 removed and 8LN2 not 2LN2
+    # spread and alpha taken from Cusack JMB 1981 145, 539-541
 
-if args.output_filename != None:
-    output = open(args.output_filename,'w')
-else:
-    output = sys.stdout
+    inv_wave_no = wavelength / (2.0 * np.pi)
+    con = 4.0 * ((spread * inv_wave_no)**2 )
+    bon = divergence**2
+    aon = inv_wave_no * np.sqrt(8.0 * np.log(2.0))
 
-for i in range(0, args.n_points):
-    output.write("{0:7.4f} {1:7.4f}\n".format(q[i], scat[i]))
+    n_q = len(q)
+    n_qq = (2 * n_q) + 101
+    mid_qq = n_q + 50
+
+    vv_break = np.ceil(mid_qq / 2.0)
+
+    # Calculation of lookup table for sigma
+
+    qq = np.empty(n_qq)
+    sigma = np.empty(n_qq)
+    f = np.empty(n_qq)
+
+    qq[mid_qq] = 0.0
+    f[mid_qq] = 1.0
+
+    for ndx in range(1, mid_qq + 1):
+        neg_ndx = mid_qq - ndx
+        pos_ndx = mid_qq + ndx
+        print pos_ndx
+        qq[neg_ndx] = -ndx * q_delta
+        qq[pos_ndx] = ndx * q_delta
+        sigma[pos_ndx] = np.sqrt(con * qq[pos_ndx]**2 + bon) / aon
+        sigma[neg_ndx] = sigma[pos_ndx]
+        if ndx <= n_q:
+            f[pos_ndx] = i[ndx - 1]
+            f[neg_ndx] = i[ndx - 1]
+        else:
+            f[pos_ndx] = i[-1]
+            f[neg_ndx] = i[-1]
+
+    sigma[mid_qq] = 2.0 * sigma[mid_qq + 1] - sigma[mid_qq + 2]
+
+    g = np.empty(n_qq)
+    asum  = np.zeros(n_qq)
+
+    sqrt2pi = np.sqrt(2.0 * np.pi)
+
+    for ndx1 in range(0, n_qq):
+        # Calculation of Gaussian G
+        for ndx2 in range(0, n_qq):
+            # Note: difference from Chauvin and Cusack - ndx1 not ndx2 for aa, vv
+            aa = 1.0 / (sigma[ndx1] * sqrt2pi)
+            g[ndx2] = 0.0
+            vv = (qq[ndx1] - qq[ndx2]) / sigma[ndx1]
+            vv = vv**2
+            if vv < vv_break:
+                g[ndx2] = aa * np.exp(-vv / 2.0)
+
+        for ndx2 in range(0, n_qq):
+            asum[ndx1] += f[ndx2]*g[ndx2]
+
+        asum[ndx1] *= q_delta
+
+    for ndx in range(mid_qq + 1, mid_qq + n_q + 1):
+        ndx2 = ndx - mid_qq - 1
+        i[ndx2] = asum[ndx]
+
+def main():
+
+    args = parse_arguments()
+
+    res_freq, coords = p2s.read_pdb_atom_data(args.input_filename)
+
+    q, scat = spheres_to_sas_curve(coords, args.radius, args.q_max, args.n_points, rbins = args.n_bins)
+
+    if args.smear:
+        q_delta = args.q_max / args.n_points
+        smear_curve(scat, q, q_delta, args.wavelength, args.spread, args.divergence)
+
+    if args.output_filename != None:
+        output = open(args.output_filename,'w')
+    else:
+        output = sys.stdout
+
+    for i in range(0, args.n_points):
+        output.write("{0:7.4f} {1:7.4f}\n".format(q[i], scat[i]))
+
+if __name__ == "__main__":
+    main()
