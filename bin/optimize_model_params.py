44212c39d911ae4099da4fc20537c68608694141
dave <none@none>
2013-12-17 09:34:54 +0000

    Added files to run full analysis on PDB files - i.e. generate dry and wet sphere models and calculate theoretical scattering curves, then compare these to experimental values. optmize_model_params - optimizes the box side and cutoff used to create models sct_pdb_analysis - runs full analysis on a directory of PDBs

---
+++ bin/optimize_model_params.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Take an input PDB and optimize the box size and cutoff used to create sphere
+models. The optimization is run to recreate the volume predicted from sluv2.
+The box size determines the size of the grid used to create the sphere models
+from a PDB. The cutoff is used in the creation of the hydration layer.
+
+These parameters can then be put into the input file (yaml) for the
+analyse_pdb_models.py script to run on a series of models.
+"""
+
+import argparse
+import pdb2sphere as p2s
+import hydrate_spheres as hydrate
+import get_boxside
+import sluv2
+import yaml
+
+def parse_arguments():
+    """Parse command line arguments and ensure correct combinations present"""
+
+    parser = argparse.ArgumentParser(description= 'Optimize the box side used to create dry sphere models and cutoff used to creat a hydration layer that reproduce expected volumes from an input PDB.\n')
+
+    parser.add_argument('-i','--input_pdb', nargs='?', type=str,
+        help = 'Path to the input PDB file', required=True)
+
+    parser.add_argument('-o','--output_file', nargs='?', type=str,
+        default='.', help = 'Path in which to save output files')
+
+    parser.add_argument('-p','--parameter_file', nargs='?', type=str,
+        help = 'Path to a file containing input parameters', required=True)
+
+    parser.add_argument('-m','--min_max', nargs=2, type=float,
+        default = [2.0, 11.0], help = 'Path to a file containing input parameters')
+
+    return parser.parse_args()
+
+args = parse_arguments()
+
+# Read in parameters
+param_file = file(args.parameter_file)
+param = yaml.load(param_file)
+
+# Read in initial PDB
+res_freq, atom_coords = p2s.read_pdb_atom_data(args.input_pdb)
+
+# Get volume from sluv for PDB
+volume = sluv2.sum_volume(sluv2.all_residues, res_freq, 'perkins1986b')
+
+# Optimize box side for sphere models
+cutoff = param['sphere']['cutoff']
+box_side, err = get_boxside.optimize_side(cutoff, atom_coords, volume,
+                                          args.min_max[0],
+                                          args.min_max[1],
+                                          0.01)
+
+dry_spheres, x_axis, y_axis, z_axis = p2s.create_sphere_model(atom_coords,
+                                                              cutoff,
+                                                              box_side)
+
+# Calculate the expected volume of the hydration layer
+# Hydration is estimated to be 0.3 g water per 1 g protein
+# Bound water volume (< bulk volume) is given as a sluv parameter
+water_vol = sluv2.calc_model_wet_volume(res_freq)
+
+wet_volume = volume + water_vol
+
+hydr_cutoff, err = hydrate.optimize_cut(box_side,
+                                        dry_spheres,
+                                        26,
+                                        wet_volume,
+                                        10,
+                                        14,
+                                        0.01)
+
+print "Target dry volume: {0:7.4f}".format(volume)
+print "Target hydrated volume: {0:7.4f}".format(wet_volume)
+print "Sphere model box side: {0:7.4f}".format(box_side)
+print "Hydration cutoff: {0:d}".format(hydr_cutoff)
---
+++ bin/sct_pdb_analysis.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Compares theorerical scattering curves generated from PDB structure files to
+experimental x-ray and neutron scattering curves.
+
+PDBs are converted into sphere models and the Debye equation used to compute
+the theoretical curves. The original sphere models are surrounded with a
+hydration layer of spheres before the creation of curves to be compared to x-ray
+data.
+
+Within the Perkins lab this replaces the do_curve script
+"""
+
+import argparse
+import sys
+import os
+import glob
+import pdb2sphere as p2s
+import sphere2pdb as s2pdb
+import hydrate_spheres as hydrate
+import sas_curve_analysis
+import calculate_curve
+import calculate_rfactor as rfactor
+import sphere_rg
+import yaml
+import numpy as np
+
+def parse_arguments():
+    """Parse command line arguments and ensure correct combinations present"""
+
+    parser = argparse.ArgumentParser(
+        description= 'Compare theoretical curves generated from PDB files to experimental SAS curves\n')
+
+    parser.add_argument('-i','--input_path', nargs='?', type=str,
+        help = 'Path to the input PDB files', required=True)
+
+    parser.add_argument('-o','--output_path', nargs='?', type=str,
+        default='.', help = 'Path in which to save output files')
+
+    parser.add_argument('-p','--parameter_file', nargs='?', type=str,
+        help = 'Path to a file containing input parameters', required=True)
+
+    parser.add_argument('-x','--xray', nargs='?', type=str,
+        help = 'Path to a file containing experimental x-ray scattering curve', default = None)
+
+    parser.add_argument('-n','--neutron', nargs='?', type=str,
+        help = 'Path to a file containing experimental neutron scattering curve', default = None)
+
+    parser.add_argument('-t','--title', nargs='?', type=str,
+        help = 'Title to use for summary output file', default = 'sct_output')
+
+    args = parser.parse_args()
+
+    if (args.neutron == None) and (args.xray == None):
+        print "At least one experimental curve is required for comparison (xray, neutron or both).\n"
+        sys.exit(1)
+
+    return args
+
+def format_qi_data(q,i):
+    """Convert separate lists/arrays of q and i values into a single 2d array"""
+    data = []
+
+    for element in range(0, len(q)):
+        data.append([q[element], i[element]])
+
+    return np.array(data)
+
+def analyse_model(model, expt_curve, sphere_radius, param):
+    """Take a sphere model and calculate the theoretical scattering curve. The
+    Rg and Rxs1 are also calculated from the curve and returned alongside the
+    rfac comparing the theoretical curve to an experimental one."""
+    result = {}
+
+    # Calculate Rg from sphere model
+    result['model_rg'] = sphere_rg.sphere_model_rg(model, sphere_radius)
+    # Create theoretical curve from model
+    result['q'], result['i'] = calculate_curve.spheres_to_sas_curve(model,
+                                                sphere_radius,
+                                                param['curve']['qmax'],
+                                                param['curve']['npoints'],
+                                                rbins = param['curve']['radbins'])
+
+#    theor_curve = [result['q'], result['i']]
+    theor_curve = format_qi_data(result['q'], result['i'])
+    # Rg and Rxs from theoretical curve
+    result['curve_rg'], result['curve_rxs'] = get_curve_descriptors(theor_curve,
+                                                            param['rg']['fitmin'],
+                                                            param['rg']['fitmax'],
+                                                            param['rxs1']['fitmin'],
+                                                            param['rxs1']['fitmax'])
+
+    # Calculate Rfactor for theoretical vs experimental curves
+    result['rfac'] = rfactor.calculate_rfactor(expt_curve,
+                                               theor_curve,
+                                               param['rfac']['qmin'],
+                                               param['rfac']['qmax'])
+
+    return result
+
+def get_curve_descriptors(data, rg_min, rg_max, rxs_min, rxs_max):
+    """Calculate the Rg and Rxs1 from the input curve"""
+
+    # Create mask to select range of Q values for Rg fitting
+    rg_mask = (data[:,0] > rg_min) & (data[:,0] < rg_max)
+    # Create mask to select range of Q values for Rxs fitting
+    rxs_mask = (data[:,0] > rxs_min) & (data[:,0] < rxs_max)
+
+    x = data[:,0]**2
+    y_rg = np.log(data[:,1])
+    y_rxs = np.log(data[:,1] * data[:,0])
+
+    rg_result = sas_curve_analysis.guinier_fit(x[rg_mask], y_rg[rg_mask], 'rg')
+    rxs_result = sas_curve_analysis.guinier_fit(x[rxs_mask], y_rxs[rxs_mask], 'rxs1')
+
+    return rg_result['r'], rxs_result['r']
+
+args = parse_arguments()
+
+# Read in parameters
+param_file = file(args.parameter_file)
+param = yaml.load(param_file)
+
+# Create output directory and open file for summary output
+if not os.path.exists(args.output_path):
+    os.makedirs(args.output_path)
+
+summary_name = os.path.join(args.output_path, args.title + '.sum')
+summary_data = open(summary_name,'w')
+
+# Read in experimental curves and calculate Rg and Rxs
+# Setup output directories for theoretical curves and sphere models
+
+if args.neutron is not None:
+
+    neut_data = rfactor.load_scatter_curve(args.neutron,
+                                           param['rfac']['qmin'],
+                                           param['rfac']['qmax'])
+    neut_rg, neut_rxs = get_curve_descriptors(neut_data,
+                                              param['rg']['fitmin'],
+                                              param['rg']['fitmax'],
+                                              param['rxs1']['fitmin'],
+                                              param['rxs1']['fitmax'])
+
+    neut_expt_summ = "{0:7.4f} {1:7.4f}".format(neut_rg, neut_rxs)
+
+    scn_path = os.path.join(args.output_path, 'neutron','curves')
+    if not os.path.exists(scn_path):
+        os.makedirs(scn_path)
+
+    dry_model_path = os.path.join(args.output_path, 'neutron','models')
+    if not os.path.exists(dry_model_path):
+        os.makedirs(dry_model_path)
+
+else:
+    neut_expt_summ = "NA NA"
+
+
+if args.xray is not None:
+
+    xray_data = rfactor.load_scatter_curve(args.xray,
+                                           param['rfac']['qmin'],
+                                           param['rfac']['qmax'])
+    xray_rg, xray_rxs = get_curve_descriptors(xray_data,
+                                              param['rg']['fitmin'],
+                                              param['rg']['fitmax'],
+                                              param['rxs1']['fitmin'],
+                                              param['rxs1']['fitmax'])
+
+    xray_expt_summ = "{0:7.4f} {1:7.4f}".format(xray_rg, xray_rxs)
+
+    scx_path = os.path.join(args.output_path, 'xray','curves')
+    if not os.path.exists(scx_path):
+        os.makedirs(scx_path)
+
+    wet_model_path = os.path.join(args.output_path, 'xray','models')
+    if not os.path.exists(wet_model_path):
+        os.makedirs(wet_model_path)
+
+else:
+    xray_expt_summ = "NA NA"
+
+# Get list of PDBs in the input directory
+pdb_filter = os.path.join(args.input_path, '*.pdb')
+pdb_files = glob.glob(pdb_filter)
+
+if len(pdb_files) < 1:
+    print "No PDB files found to analyze"
+    sys.exit(1)
+
+# Read in initial PDB
+res_freq, atom_coords = p2s.read_pdb_atom_data(pdb_files[0])
+
+# Parameters for turning PDB into spheres
+cutoff = param['sphere']['cutoff']
+box_side = param['sphere']['boxside']
+
+# Model spheres just fit into grid box (no overlap) therefore:
+radius = box_side / 2.0
+
+# Loop over input PDBs
+for pdb in pdb_files:
+
+    pdb_basename = os.path.basename(pdb)
+    pdb_id = os.path.splitext(pdb_basename)[0]
+
+    # Read PDB
+    res_freq, atom_coords = p2s.read_pdb_atom_data(pdb)
+
+    # PDB to dry sphere model
+    dry_spheres, x_axis, y_axis, z_axis = p2s.create_sphere_model(atom_coords,
+                                                                  cutoff,
+                                                                  box_side)
+
+    # If neutron data provided compare with curve computed from dry sphere model
+    if args.neutron is not None:
+
+        neut_theor = analyse_model(dry_spheres, neut_data, radius, param)
+
+        # Write curve to file
+        curve_file = os.path.join(scn_path, pdb_id + '.scn')
+        output = open(curve_file,'w')
+        calculate_curve.output_sas_curve(neut_theor['q'],neut_theor['i'], output)
+        output.close()
+
+        # Write model to file
+        model_file = os.path.join(dry_model_path,  pdb_id + '.pdb')
+        s2pdb.write_sphere_pdb(dry_spheres, radius, model_file)
+
+        neut_summ = "{0:7.4f} {1:7.4f} {2:7.4f} {3:7.4f}".format(neut_theor['model_rg'],
+                                                                 neut_theor['curve_rg'],
+                                                                 neut_theor['curve_rxs'],
+                                                                 neut_theor['rfac'][0])
+    else:
+        neut_summ = "NA NA NA NA"
+
+    # If x-ray data provided compare with curve computed from wet sphere model
+    if args.xray is not None:
+        # Hydrate model - > wet sphere model
+        wet_spheres = hydrate.hydrate_model(dry_spheres,
+                                            param['hydrate']['positions'],
+                                            box_side,
+                                            param['hydrate']['cutoff'],
+                                            xaxis = x_axis,
+                                            yaxis = y_axis,
+                                            zaxis = z_axis)
+
+        xray_theor = analyse_model(wet_spheres, xray_data, radius, param)
+
+        # Write curve to file
+        curve_file = os.path.join(scx_path, pdb_id + '.scx')
+        output = open(curve_file,'w')
+        calculate_curve.output_sas_curve(xray_theor['q'],xray_theor['i'], output)
+        output.close()
+
+        # Write model to file
+        model_file = os.path.join(wet_model_path,  pdb_id + '.pdb')
+        s2pdb.write_sphere_pdb(wet_spheres, radius, model_file)
+
+        xray_summ = "{0:7.4f} {1:7.4f} {2:7.4f} {3:7.4f}".format(xray_theor['model_rg'],
+                                                                 xray_theor['curve_rg'],
+                                                                 xray_theor['curve_rxs'],
+                                                                 xray_theor['rfac'][0])
+    else:
+        xray_summ = "NA NA NA NA"
+
+    summary_data.write('{0:s} {1:s} {2:s} {3:s} {4:s}\n'.format(pdb_id,
+                                                                neut_summ,
+                                                                neut_expt_summ,
+                                                                xray_summ,
+                                                                xray_expt_summ,))
+
+summary_data.close()
