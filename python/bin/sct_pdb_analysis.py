5a60d2925535b64a6676fee063e10ac6f08bbc1f
dave <none@none>
2014-01-31 12:33:56 +0000

    First effort at adding Apache 2.0 license to the code. Begin with the Python code which was all written by myself (David W. Wright).

--- python/bin/sct_pdb_analysis.py
+++ python/bin/sct_pdb_analysis.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """Compares theorerical scattering curves generated from PDB structure files to
 experimental x-ray and neutron scattering curves.
 
@@ -11,6 +10,20 @@ data.
 
 Within the Perkins lab this replaces the do_curve script
 """
+
+# Copyright 2014 David W. Wright
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import sys
