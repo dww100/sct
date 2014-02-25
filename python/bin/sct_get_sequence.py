d9eb31855419a84c291630952a6c7d939bd3c53a
Dave Wright <dave.william.wright@gmail.com>
2014-02-25 11:49:14 +0000

    Fixed sct_get_sequence - removed extra \n and edited to read output filename option correctly

--- python/bin/sct_get_sequence.py
+++ python/bin/sct_get_sequence.py
@@ -36,7 +36,7 @@ def parse_arguments():
     parser.add_argument('-p', '--pdb', action='store_true', default=False,
         help = 'Flag to indicate input is a PDB rather than fasta file')
     parser.add_argument('-o','--output_filename', nargs='?', type=str,
-        dest='output_filename', help = 'Path to the output file')
+        dest='output_filename', default=None, help = 'Path to the output file')
 
     return parser.parse_args()
 
@@ -45,15 +45,15 @@ args = parse_arguments()
 # Read input
 # Input should either be PDB or fasta format - chosen by input flag
 if args.pdb:
-    res_freq, coords = sct.pdb.pdb_res_freq(args.input_filename)
+    res_freq, coords = sct.pdb.read_pdb_atom_data(args.input_filename)
 else:
     res_freq = sct.seq.fasta_res_freq(args.input_filename)
 
-if args.output_file != None:
-    sys.stdout = open(args.output_file,'w')
+if args.output_filename != None:
+    sys.stdout = open(args.output_filename,'w')
 
 # Output pdb frequencies in a yaml style format that can be read by SLUV2
 for res_name in sct.seq.amino_acids:
-    print res_name + ': ' + str(res_freq[res_name]) + '\n'
+    print res_name + ': ' + str(res_freq[res_name])
 for res_name in sct.seq.monosaccharides:
-    print res_name + ': ' + str(res_freq[res_name]) + '\n'
+    print res_name + ': ' + str(res_freq[res_name])
